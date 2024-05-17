3ea6ee09a3f0f56d1a7c18a14714345575ae3c29
Elbaraa Abuaraki <arkeez.vault@gmail.com>
2024-05-17 16:57:08 +0300

    print book report

--- main.py
+++ main.py
@@ -14,10 +14,44 @@ def count_character_occourance(text):
 def process_text_body(text):
     return re.sub(r'[^a-zA-Z]', '', text.lower())
 
-def main():
-    with open("books/frankenstein.txt") as f:
+def dict_to_list(characters_dict):
+    character_list = []
+    
+    for key in characters_dict:
+        character_list.append({ "character": key, "occurance": characters_dict[key] })
+    
+    return character_list
+
+def sort_on(dict):
+    return dict["occurance"]
+
+def get_text_body(file):
+    with open(file) as f:
         file_contents = f.read()
         file_contents = process_text_body(file_contents)
-        count_character_occourance(file_contents)
         
+        return file_contents
+
+def count_text_words(file):    
+    with open(file) as f:
+        file_contents = f.read()
+        
+        return len(file_contents.strip().split(" "))
+def main():
+    file = "books/frankenstein.txt"
+    file_contents = get_text_body(file)
+        
+    characters_by_occurance = count_character_occourance(file_contents)
+    character_list = dict_to_list(characters_by_occurance)
+    character_list.sort(reverse=True, key=sort_on)
+    word_count = count_text_words(file)
+    
+    print("--- Begin report of books/frankenstein.txt --")
+    print(f"{word_count} words found in the document")
+    print("")
+    
+    for char in character_list:
+        print(f"The '{char["character"]}' character was found {char["occurance"]} times")
+    
+    print("--- End report ---")
 main()
